63e08bef79034b420dd51c06faa6ece5d99544d6
Halim Choukani <halim.choukani@gmail.com>
2024-11-11 19:21:25 +0100

    Refactor app.py by removing unused imports and cleaning up code

--- app.py
+++ app.py
@@ -10,14 +10,10 @@ import math
 import cv2 as cv
 import numpy as np
 import mediapipe as mp
-
 from utils import CvFpsCalc
 from model import KeyPointClassifier
-from model import PointHistoryClassifier
-
 import pyautogui
 import keyboard
-import time
 
 def resetZoom():
     pyautogui.hotkey('ctrl', '0')
